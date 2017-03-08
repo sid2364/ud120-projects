6af3552dc6a9fe1ede1b0ac3bbcdc31f0ebfbe0e
Siddharth Sahay <sahaysid@gmail.com>
2017-03-08 08:37:42 +0000

    Feature scaling + KMeans quiz

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -63,18 +63,29 @@ print "Min: ", str(vmin), "\nMax: ", str(vmax)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+#feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+from sklearn.preprocessing import MinMaxScaler
+finance_features.append(numpy.array([200000, 1000000]))
+
+scaler = MinMaxScaler()
+transformed_finance_features = scaler.fit_transform(finance_features)
+
+#for i in range(len(transformed_finance_features)):
+#	print finance_features[i], " => ", transformed_finance_features[i]
+
+numpy.delete(finance_features, len(finance_features)-1)
+numpy.delete(transformed_finance_features, len(transformed_finance_features)-1)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
